8e4148aeddb618c3fb374faf317113e538d3f2ee
xieyikang <964948274@qq.com>
2022-10-28 10:52:07 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author xykstart
+ * @create 2022-10-28 10:49
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
